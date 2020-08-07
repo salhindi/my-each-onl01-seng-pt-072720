42127ee59a00dc47f360b43e9d40a181d2d74cd0
Sara A <calisaraalhindi@gmail.com>
2020-08-07 21:33:41 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,7 @@ def my_each(words) # put argument(s) here
 words = "hi", "hello", "bye", "goodbye"  # code here
 while i < words.length
 end
-  yields
+  yield(words[i])
   i = 0
   while my_each do |item|
     item = i 
