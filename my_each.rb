0e360e48b138ca931d9b189b16d89a95b64d4a78
Sara A <calisaraalhindi@gmail.com>
2020-08-07 21:33:24 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,6 @@
 def my_each(words) # put argument(s) here
 words = "hi", "hello", "bye", "goodbye"  # code here
-while my_each do |words|
-  words.length.times
+while i < words.length
 end
   yields
   i = 0
